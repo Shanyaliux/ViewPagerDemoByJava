fa58373f2ce414f6d1e518c743d1cc2454f9fb0e
Shanyaliux <445502929@qq.com>
2020-09-18 19:18:56 +0800

    init

---
+++ app/src/main/java/world/shanya/viewpagerdemobyjava/MainActivity.java
@@ -0,0 +1,64 @@
+package world.shanya.viewpagerdemobyjava;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager2 = findViewById(R.id.viewPager2);
+        viewPager2.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                switch (position){
+                    case 0:
+                        return new OneFragment();
+                    case 1:
+                        return new TwoFragment();
+                    case 2:
+                        return new ThreeFragment();
+                    default:
+                }
+                return new OneFragment();
+            }
+
+            @Override
+            public int getItemCount() {
+                return 3;
+            }
+        });
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0:
+                        tab.setText("One");
+                        break;
+                    case 1:
+                        tab.setText("Two");
+                        break;
+                    case 2:
+                        tab.setText("Three");
+                        break;
+                }
+            }
+        });
+
+        tabLayoutMediator.attach();
+    }
+}
